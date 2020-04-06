ceb2dfda22abf6859ee44b3708c38b96ad386c0a
gux <gux.vega@gmail.com>
2020-04-06 11:41:41 -0600

    Updates

---
+++ precache-manifest.0a1b877324d4532cc2b251819df9d7c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc23a5dba22c4a0b64db70c13581ac94",
+    "url": "/code-splitting-app/index.html"
+  },
+  {
+    "revision": "6fccab667f424d7e375b",
+    "url": "/code-splitting-app/static/css/main.92905b29.chunk.css"
+  },
+  {
+    "revision": "50f7406560306e56557e",
+    "url": "/code-splitting-app/static/js/2.32edf3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-splitting-app/static/js/2.32edf3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f311bdfcddc1b162dcf2",
+    "url": "/code-splitting-app/static/js/3.1deba769.chunk.js"
+  },
+  {
+    "revision": "a55bf647181dfbc3ed2f",
+    "url": "/code-splitting-app/static/js/4.d45f6652.chunk.js"
+  },
+  {
+    "revision": "6fccab667f424d7e375b",
+    "url": "/code-splitting-app/static/js/main.236006dd.chunk.js"
+  },
+  {
+    "revision": "7f0b829203a28daa333b",
+    "url": "/code-splitting-app/static/js/runtime-main.f53991e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code-splitting-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
